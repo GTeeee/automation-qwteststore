d8578d2a4bc63188826e9683ce7da84d6d0a8228
GTeeee <gtodd@qualityworkscg.com>
2022-02-08 21:05:50 -0500

    Create checkout test file

---
+++ test/checkout.test.js
@@ -0,0 +1,23 @@
+describe('Cart checkout test cases ', function(){
+
+    it('Valid email and pw ', function(){
+
+    })
+
+    it('Valid email and invalid pw ', function(){
+
+    })
+
+    it('Valid email and null password', function(){
+
+    })
+
+    it('Invalid email and pw', function(){
+
+    })
+
+    it('No email or password', function(){
+
+    })
+
+})
