02e7fc966a7dacff10796b9c6049fc89077f5c0d
GTeeee <gtodd@qualityworkscg.com>
2022-03-01 09:57:36 -0500

    Complete all five test cases

--- pages/cart.page.js
+++ pages/cart.page.js
@@ -1,7 +1,23 @@
 class shoppingCart{
-    
 
+    get priceInCart(){ return $('div.meta')} //this gets back the quantity and price
+    get subTotalInCart(){ return $(`div.ui.large.clearing.segment > span:nth-child(1)`)}
+    get signOutButton(){ return $(`div.right.menu a:nth-child(2)`)}
 
+    getTextOfPrice(){
+        this.priceInCart.waitForDisplayed()
+        return this.priceInCart.getText()
+    }
+
+    signOutOfAccount(){
+        this.signOutButton.waitForDisplayed()
+        this.signOutButton.click()
+    }
+
+    getSubtotalText(){
+        this.subTotalInCart.waitForDisplayed()
+        return this.subTotal.cart.getText()
+    }
 
 }
 module.exports = new shoppingCart();
--- test/addToCart.test.js
+++ test/addToCart.test.js
@@ -1,33 +1,33 @@
 cart = require('../pages/cart.page.js')
 productPage = require('../pages/product.page.js')
-signInPage = require('../pages/signInPage.js')
+signInPage = require('../pages/signIn.page.js')
 
 describe('shopping cart test as guest and authenicated user', function(){
     it('should add one item to cart - guest', async () => {
         await browser.url(`${browser.options.baseUrl}`)
         await productPage.seeSingleItem(1)
-        await productPage.addItemToCart(1)
+        await productPage.addItemToCart()
         expect(productPage.shoppingCartSuccess).to.be.displayed
 
     })
     it('should add multiple items to the cart - guest', async () => {
         await browser.url(`${browser.options.baseUrl}`)
         await productPage.seeSingleItem(1)
-        await productPage.addItemToCart(1)
+        await productPage.addItemToCart()
         expect(productPage.shoppingCartSuccess).to.be.displayed
         await browser.back()
         await productPage.seeSingleItem(4)
-        await productPage.addItemToCart(4)
+        await productPage.addItemToCart()
         expect(productPage.shoppingCartSuccess).to.be.displayed
     })
     it('should add one item to cart multiple times - guest', async () => {
         await browser.url(`${browser.options.baseUrl}`)
         await productPage.seeSingleItem(3)
-        await productPage.addItemToCart(3)
+        await productPage.addItemToCart()
         expect(productPage.shoppingCartSuccess).to.be.displayed
         await browser.back()
         await productPage.seeSingleItem(3)
-        await productPage.addItemToCart(3)
+        await productPage.addItemToCart()
         expect(productPage.shoppingCartSuccess).to.be.displayed
         //await browser.url(`${browser.options.baseUrl}/cart`)
     })
@@ -37,11 +37,31 @@ describe('shopping cart test as guest and authenicated user', function(){
         await signInPage.addPasswordToField('desk1')
         await signInPage.clickLoginButton()
         await browser.url(`${browser.options.baseUrl}`)
-        
+        await productPage.seeSingleItem(5)
+        await productPage.getPriceOfProduct()
+        await productPage.addItemToCart()
+        await browser.url(`${browser.options.baseUrl}/cart`)
+        expect (cart.getTextOfPrice).to.contain(productPage.getPriceOfProduct)
 
     })
-    it.skip('should ensure user cart does not change after sign out ', async () => {
-
+    it('should ensure user cart does not change after sign out ', async () => {
+        await browser.url(`${browser.options.baseUrl}/login`)
+        await signInPage.addEmailToField('desk@desk.com')
+        await signInPage.addPasswordToField('desk1')
+        await signInPage.clickLoginButton()
+        await browser.url('/')
+        await productPage.seeSingleItem(1)
+        await productPage.addItemToCart()
+        //await productPage.addItemToCart()
+        //await productPage.addItemToCart()
+        await browser.url(`${browser.options.baseUrl}/cart`)
+        let preSignOut = await cart.getSubtotalText
+        await cart.signOutOfAccount()
+        await signInPage.addEmailToField('desk@desk.com')
+        await signInPage.addPasswordToField('desk1')
+        await signInPage.clickLoginButton()
+        await browser.url(`${browser.options.baseUrl}/cart`)
+        expect (preSignOut).to.equal(cart.getSubtotalText)
 
     })
 
